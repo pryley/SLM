6fa4135116caad38f399eb68cac6264c30e6c295
Paul Ryley <paul@geminilabs.io>
2017-08-27 20:45:03 -0400

    add custom deploy tasks

--- deploy.php
+++ deploy.php
@@ -2,28 +2,36 @@
 
 namespace Deployer;
 
-require __DIR__ . '/deploy/recipe/common.php';
-require __DIR__ . '/deploy/recipe/lumen.php';
-// require __DIR__ . '/deploy/recipe/common/prepare.php';
-// require __DIR__ . '/deploy/recipe/common/rollback.php';
-// require __DIR__ . '/deploy/recipe/common/symlink.php';
+require_once __DIR__ . '/deploy/recipe/common.php';
+require_once __DIR__ . '/deploy/recipe/lumen.php';
 
 configuration( 'deploy/config.yml' );
 inventory( 'deploy/hosts.yml' );
 
 after( 'deploy:failed', 'deploy:unlock' );
 
+desc( 'Execute artisan slm:install' );
+task( 'slm:install', function() {
+	$output = run( '{{bin/php}} {{release_path}}/artisan slm:install' );
+	writeln( '<info>' . $output . '</info>' );
+});
+
+desc( 'Execute artisan slm:clients' );
+task( 'slm:clients', function() {
+	$output = run( '{{bin/php}} {{release_path}}/artisan slm:clients' );
+	writeln( '<info>' . $output . '</info>' );
+});
+
+desc( 'Execute artisan slm:users' );
+task( 'slm:users', function() {
+	$output = run( '{{bin/php}} {{release_path}}/artisan slm:users' );
+	writeln( '<info>' . $output . '</info>' );
+});
+
+// dep ssh
 // dep artisan:migrate [stage]
 // dep artisan:migrate:rollback [stage]
 // dep artisan:migrate:status [stage]
 // dep artisan:db:seed [stage]
 // dep artisan:cache:clear [stage]
 // dep artisan:queue:restart [stage]
-
-// desc('Restart PHP-FPM service');
-// task('php-fpm:restart', function () {
-//     // The user must have rights for restart service
-//     // /etc/sudoers: username ALL=NOPASSWD:/bin/systemctl restart php-fpm.service
-//     run('sudo systemctl restart php-fpm.service');
-// });
-// after('deploy:symlink', 'php-fpm:restart');
