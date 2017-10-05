c0730b3e9f86e0033a5f548e2d2260576c90d3e1
Paul Ryley <paul@geminilabs.io>
2017-10-05 12:45:47 -0600

    fix test race conditons

--- tests/Feature/ManageLicenseDomainsTest.php
+++ tests/Feature/ManageLicenseDomainsTest.php
@@ -24,16 +24,15 @@ class ManageLicenseDomainsTest extends TestCase
 	/** @test */
 	public function add_a_domain_to_a_license()
 	{
-		// $this->actingAs( factory( User::class )->create(), 'api' );
-
 		$license = factory( License::class )->create();
 		$this->auth()->post( '/v1/domains', $this->validParams([
 			'domain' => 'www.test.com',
 			'license_key' => $license->license_key,
-		]));
+		]))->seeJson([
+			'status' => 200,
+		]);
 
 		// error_log( print_r( $this->response, 1 ));
-		$this->assertResponseStatus( 200 );
 
 		// get License domains
 		// $domain = $this->getDomainFromLicense( $this->response->original );
@@ -43,39 +42,32 @@ class ManageLicenseDomainsTest extends TestCase
 		// add domain to License
 	}
 
-	/** @test */
 	public function domain_field_is_required()
 	{
 	}
 
-	/** @test */
 	public function domain_field_must_be_unique_for_license()
 	{
 	}
 
-	/** @test */
 	public function domain_field_must_exist_to_be_removed_from_a_license()
 	{
 	}
 
-	/** @test */
 	public function get_all_domains_of_a_license()
 	{
 		// get License from license_key
 		// return License->domains
 	}
 
-	/** @test */
 	public function license_key_field_is_required()
 	{
 	}
 
-	/** @test */
 	public function license_key_field_must_be_valid()
 	{
 	}
 
-	/** @test */
 	public function remove_a_domain_from_a_license()
 	{
 		// get License from license_key
--- tests/Feature/ManageLicensesTest.php
+++ tests/Feature/ManageLicensesTest.php
@@ -30,8 +30,9 @@ class ManageLicensesTest extends TestCase
 	{
 		$this->auth()->post( '/v1/licenses', $this->validParams([
 			'software' => factory( Software::class )->create()->slug,
-		]));
-		$this->assertResponseStatus( 200 );
+		]))->seeJson([
+			'status' => 200,
+		]);
 	}
 
 	/** @test */
@@ -43,24 +44,31 @@ class ManageLicensesTest extends TestCase
 			'last_name' => '',
 			'software' => '',
 			'transaction_id' => '',
-		]));
-		$errors = $this->response->getData()->errors;
-		$this->assertContains( 'The email field is required.', $errors->email );
-		$this->assertContains( 'The first name field is required.', $errors->first_name );
-		$this->assertContains( 'The last name field is required.', $errors->last_name );
-		$this->assertContains( 'The software field is required.', $errors->software );
-		$this->assertContains( 'The transaction id field is required.', $errors->transaction_id );
-		$this->assertResponseStatus( 422 );
+		]))->seeJson([
+			'status' => 422,
+			'errors' => (object) [
+				'email' => ['The email field is required.'],
+				'first_name' => ['The first name field is required.'],
+				'last_name' => ['The last name field is required.'],
+				'software' => ['The software field is required.'],
+				'transaction_id' => ['The transaction id field is required.'],
+			],
+		]);
 	}
 
 	/** @test */
 	public function email_field_must_be_valid()
 	{
+		$software = factory( Software::class )->create();
 		$this->auth()->post( '/v1/licenses', $this->validParams([
+			'software' => $software->slug,
 			'email' => 'invalid email',
-		]));
-		$this->assertContains( 'The email must be a valid email address.', $this->response->getData()->errors->email );
-		$this->assertResponseStatus( 422 );
+		]))->seeJson([
+			'status' => 422,
+			'errors' => (object) [
+				'email' => ['The email must be a valid email address.']
+			],
+		]);
 	}
 
 	/** @test */
@@ -68,9 +76,12 @@ class ManageLicensesTest extends TestCase
 	{
 		$this->auth()->post( '/v1/licenses', $this->validParams([
 			'software' => 'invalid_software_slug',
-		]));
-		$this->assertContains( 'The selected software is invalid.', $this->response->getData()->errors->software );
-		$this->assertResponseStatus( 422 );
+		]))->seeJson([
+			'status' => 422,
+			'errors' => (object) [
+				'software' => ['The selected software is invalid.']
+			],
+		]);
 	}
 
 	/** @test */
@@ -82,9 +93,12 @@ class ManageLicensesTest extends TestCase
 			'transaction_id' => 'transaction_id',
 		]);
 		$this->auth()->post( '/v1/licenses', $data );
-		$this->auth()->post( '/v1/licenses', $data );
-		$this->assertContains( 'The transaction id has already been taken.', $this->response->getData()->errors->transaction_id );
-		$this->assertResponseStatus( 422 );
+		$this->auth()->post( '/v1/licenses', $data )->seeJson([
+			'status' => 422,
+			'errors' => (object) [
+				'transaction_id' => ['The transaction id has already been taken.']
+			],
+		]);
 	}
 
 	/** @test */
@@ -95,9 +109,10 @@ class ManageLicensesTest extends TestCase
 		]);
 		$this->auth()->post( '/v1/licenses/deactivate', [
 			'license_key' => $license->license_key,
+		])->seeJson([
+			'status' => 200,
 		]);
 		$this->assertEquals( 'inactive', $this->response->getData()->data->status );
-		$this->assertResponseStatus( 200 );
 	}
 
 	/** @test */
@@ -106,17 +121,19 @@ class ManageLicensesTest extends TestCase
 		$license = factory( License::class )->create([
 			'status' => 'active',
 		]);
-		$this->auth()->delete( '/v1/licenses/'.$license->license_key );
-		$this->assertResponseStatus( 204 );
+		$this->auth()->delete( '/v1/licenses/'.$license->license_key )->seeJson([
+			'status' => 204,
+		]);
 	}
 
 	/** @test */
 	public function get_all_licenses()
 	{
 		factory( License::class, 5 )->create();
-		$this->auth()->get( '/v1/licenses' );
+		$this->auth()->get( '/v1/licenses' )->seeJson([
+			'status' => 200,
+		]);
 		$this->assertNotEmpty( $this->response->getData()->data );
-		$this->assertResponseStatus( 200 );
 	}
 
 	/** @test */
@@ -130,8 +147,9 @@ class ManageLicensesTest extends TestCase
 		$this->post( '/v1/licenses/verify', [
 			'license_key' => $license->license_key,
 			'software' => $software->slug,
+		])->seeJson([
+			'status' => 200,
 		]);
-		$this->assertResponseStatus( 200 );
 	}
 
 	/** @test */
@@ -145,6 +163,8 @@ class ManageLicensesTest extends TestCase
 		$this->auth()->post( '/v1/licenses/renew', [
 			'license_key' => $license->license_key,
 			'max_domains_allowed' => 5,
+		])->seeJson([
+			'status' => 200,
 		]);
 		$data = $this->response->getData()->data;
 		$this->assertTrue( Carbon::parse( $data->renewedAt )->isToday() );
@@ -152,7 +172,6 @@ class ManageLicensesTest extends TestCase
 		$this->assertEquals( 'active', $data->status );
 		$this->assertEquals( 1, $data->numTimesRenewed );
 		$this->assertEquals( 5, $data->maxDomainsAllowed );
-		$this->assertResponseStatus( 200 );
 	}
 
 	/** @test */
@@ -164,9 +183,10 @@ class ManageLicensesTest extends TestCase
 		]);
 		$this->auth()->post( '/v1/licenses/restore', [
 			'license_key' => $license->license_key,
+		])->seeJson([
+			'status' => 200,
 		]);
 		$this->assertContains( 'active', $this->response->getData()->data->status );
-		$this->assertResponseStatus( 200 );
 	}
 
 	/** @test */
@@ -177,8 +197,9 @@ class ManageLicensesTest extends TestCase
 		]);
 		$this->auth()->post( '/v1/licenses/revoke', [
 			'license_key' => $license->license_key,
+		])->seeJson([
+			'status' => 200,
 		]);
 		$this->assertContains( 'revoked', $this->response->getData()->data->status );
-		$this->assertResponseStatus( 200 );
 	}
 }
